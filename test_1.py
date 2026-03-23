ac35e4150eb347e6196b21aaf63547393e3d50fb
Heng Bona <hengbunyi0109@gmail.com>
2026-03-23 11:33:18 +0700

    Add files via upload

---
+++ test_1.py
@@ -0,0 +1,13 @@
+a = 3
+b = 2
+c = 3
+d = 3
+if a == b and c == d:
+    print('output 1 correct')
+else:
+    print('wrong')
+
+if a == b & c == d:
+    print('output 2 correct')
+else:
+    print('wrong')
